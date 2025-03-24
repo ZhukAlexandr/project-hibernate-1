f2e26c0400f38144f984c2cc4c896b34154139d2
AlexZhuk <alexanderzhuk.93@gmail.com>
2025-03-24 22:59:02 +0300

    memory to db

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,6 +1,11 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import jakarta.persistence.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
@@ -10,42 +15,104 @@ import java.util.Optional;
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
 
-    public PlayerRepositoryDB() {
+    public static final String SELECT_FROM_PLAYER_LIMIT_OFFSET = "SELECT * FROM player   LIMIT :limit OFFSET  :offset";
+    private final SessionFactory sessionFactory;
 
+    public PlayerRepositoryDB() {
+        this.sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
     }
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+
+        Transaction tx = null;
+        try(Session session = sessionFactory.openSession();){
+            tx = session.beginTransaction();
+            Query query = session.createNativeQuery(SELECT_FROM_PLAYER_LIMIT_OFFSET, Player.class);
+            query.setParameter("offset", pageNumber * pageSize);
+            query.setParameter("limit", pageSize);
+            List<Player> players = query.getResultList();
+            tx.commit();
+            return players;
+        } catch (Exception e) {
+            tx.rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int getAllCount() {
-        return 0;
+        Transaction tx = null;
+
+        try(Session session = sessionFactory.openSession();){
+
+            tx = session.beginTransaction();
+            Query query = session.createNamedQuery("Player_Count", Long.class);
+            int playerCount = ((Long)query.getSingleResult()).intValue();
+            tx.commit();
+            return playerCount;
+        } catch (Exception e) {
+            tx.rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Player save(Player player) {
-        return null;
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession();) {
+            tx = session.beginTransaction();
+            session.persist(player);
+            tx.commit();
+            return player;
+        } catch (Exception e) {
+            tx.rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Player update(Player player) {
-        return null;
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession();) {
+            transaction = session.beginTransaction();
+            Player updatedPlayer = (Player) session.merge(player);
+            transaction.commit();
+            return updatedPlayer;
+        } catch (Exception e) {
+            transaction.rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Optional<Player> findById(long id) {
-        return Optional.empty();
+        Transaction tx = null;
+        try(Session session = sessionFactory.openSession();){
+            tx = session.beginTransaction();
+            Optional<Player> player = Optional.ofNullable((Player) session.get(Player.class, id));
+            return player;
+        } catch (Exception e){
+            tx.rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void delete(Player player) {
-
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession();) {
+            tx = session.beginTransaction();
+            session.delete(player);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @PreDestroy
     public void beforeStop() {
-
+        sessionFactory.close();
     }
 }
